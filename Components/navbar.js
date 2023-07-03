5a950257085c2654251255266ed7bce6e1ef3182
Alok Singh <askushw07@gmail.com>
2023-07-04 01:58:36 +0530

    folders created

---
+++ Components/navbar.js
@@ -0,0 +1,22 @@
+function navbar(){
+    return `
+        <nav>
+            <div>
+                <p><a href="">Green Paradise</a></p>
+            </div>
+            <div>
+                <p><a href="">Catlog</a></p>
+                <p><a href="">Sale</a></p>
+                <p><a href="">Delivery & Payment</a></p>
+                <p><a href="">Contact</a></p>
+            </div>
+            <div>
+                <p><a><i class="fa-solid fa-magnifying-glass fa-lg"></i></a></p>
+                <p><a><i class="fa-solid fa-language fa-lg"></i></a></p>
+                <p><a><i class="fa-solid fa-user fa-lg"></i></a></p>
+                <p><a><i class="fa-solid fa-cart-shopping fa-lg"></i></a></p>
+            </div>
+        </nav>`
+}
+
+export default navbar
---
+++ SCRIPTS/index.js
@@ -0,0 +1,4 @@
+import navbar from "../Components/navbar.js";
+import footer from "../Components/footer.js";
+document.getElementById("nav-bar").innerHTML=navbar();
+document.getElementById("footer").innerHTML=footer();
