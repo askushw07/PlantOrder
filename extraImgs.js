56d40ed9a117bfd10031a16685d70331ecdefbe5
DevWebAbhi <tiwariabhishek889912@gmail.com>
2023-07-04 18:56:41 +0530

    10%

---
+++ extraImgs.js
@@ -0,0 +1,11 @@
+const imgs ={
+
+        imageUrl2:"https://perenual.com/storage/marketplace/3-Whimsy%20and%20Wonder%20Seeds/p-RejW64156b576b1441/thumbnail/i-1-36ZG64156b57ec3a71.jpg",
+        imageUrl3:"https://perenual.com/storage/marketplace/3-Whimsy%20and%20Wonder%20Seeds/p-RejW64156b576b1441/thumbnail/i-2-hvHT64156b5896bff1.jpg",
+        imageUrl4:"https://perenual.com/storage/marketplace/3-Whimsy%20and%20Wonder%20Seeds/p-RejW64156b576b1441/thumbnail/i-3-0wu064156b5922e361.jpg",
+        imageUrl5:"https://perenual.com/storage/marketplace/3-Whimsy%20and%20Wonder%20Seeds/p-RejW64156b576b1441/thumbnail/i-4-Uh7364156b59a1da61.jpg",
+        imageUrl6:"https://perenual.com/storage/marketplace/3-Whimsy%20and%20Wonder%20Seeds/p-RejW64156b576b1441/thumbnail/i-5-LOOh64156b5a2d9eb1.jpg",
+        imageUrl7:"https://perenual.com/storage/marketplace/3-Whimsy%20and%20Wonder%20Seeds/p-RejW64156b576b1441/thumbnail/i-6-Gxm564156b5ae2ff11.jpg",
+        imageUrl8:"https://perenual.com/storage/marketplace/3-Whimsy%20and%20Wonder%20Seeds/p-RejW64156b576b1441/thumbnail/i-7-fKQf64156b5b89d7c1.jpg",
+
+};
