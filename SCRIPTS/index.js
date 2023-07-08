d3b01f797df3c449ba20ad39adbd41692bf85239
Alok Singh <askushw07@gmail.com>
2023-07-08 09:26:31 +0530

    Home Page Completed

--- SCRIPTS/index.js
+++ SCRIPTS/index.js
@@ -1,10 +1,39 @@
-import {navbar,fetchdata} from "../Components/navbar.js";
+import * as functions from "../Components/navbar.js";
 import footer from "../Components/footer.js";
-document.getElementById("nav-bar").innerHTML=navbar();
+
+
+document.getElementById("nav-bar").innerHTML=functions.navbar();
+let navbar=document.getElementById("navbar");
+navbar.innerHTML=functions.reponsive_nav();
 document.getElementById("footer").innerHTML=footer();
 
 
 
-fetchdata();
+//fetchdata();
 
 //let src=document.getElementById("main").querySelector(".img").querySelector("img").src;
+
+document.getElementById("item").addEventListener("keyup", function (e) {
+  if(e.target.value)
+  functions.betterFetchItems(e.target.value);
+  else{
+    document.getElementById("popup").style.display="none";
+  }
+});
+
+
+//document.getElementById("popup").addEventListener("click",myFunction);
+
+
+
+
+  document.getElementById("menu").addEventListener("click",()=>{
+    let rnav = document.getElementById("respnav");
+    let state = rnav.style.display;
+    if(!state || state=="none"){
+      rnav.style.display="flex";
+    }
+    else{
+      rnav.style.display="none";
+    }
+  })
